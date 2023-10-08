c8c2a8144b9f20c416c10c3edd63d6d11fba3f6d
mikewschmidt <michaelwschmidt@gmail.com>
2023-10-08 18:45:34 -0500

    commenting out Oracle database functions

--- db.py
+++ db.py
@@ -13,17 +13,19 @@ import boto3
 
 
 # Setting variables and creating the connection engine
-conn_str = '''(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.us-sanjose-1.oraclecloud.com))(connect_data=(service_name=ga3e236c6957ba6_oltpdb_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))'''
-user = "appuser"
-password = os.environ['ORACLE_PASSWORD_APPUSER']
-engine = create_engine(
-    f'oracle+oracledb://{user}:{password}@{conn_str}'
-)
+### NOT USING ORACLE RIGHT NOW
+#conn_str = '''(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.us-sanjose-1.oraclecloud.com))(connect_data=(service_name=ga3e236c6957ba6_oltpdb_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))'''
+#user = "appuser"
+#password = os.environ['ORACLE_PASSWORD_APPUSER']
+#engine = create_engine(
+#    f'oracle+oracledb://{user}:{password}@{conn_str}'
+#)
 
 
 # Query database with title and location
 
-
+### NOT USING ORACLE RIGHT NOW
+'''
 def query_db(title, location) -> list:
     title = title.replace(" ", "%")
     with engine.connect() as conn:
@@ -36,9 +38,11 @@ def query_db(title, location) -> list:
         # results = conn.execute(text(f"SELECT * FROM tbl_jobs WHERE job_title like '%{title}%' AND location like '%{location}%'"))
         results = df_results.to_dict("records")
     return results
-
+'''
 
 # Query database by id
+### NOT USING ORACLE RIGHT NOW
+'''
 def query_db_by_id(job_ids: np.ndarray, max_years: int) -> pd.DataFrame:
     # Convert it into a tuple or if there is 1 job ID, then just get that
     if len(job_ids) == 1:
@@ -56,7 +60,7 @@ def query_db_by_id(job_ids: np.ndarray, max_years: int) -> pd.DataFrame:
             AND max_exp <= {max_years} 
             ORDER BY posting_date DESC""", conn)
     return df_results
-
+'''
 
 # Get all the job IDs in the database, to keep it in RAM
 
@@ -80,7 +84,8 @@ def get_all_job_ids_db():
 
 # Get job details in the database by job IDs
 
-
+### NOT USING ORACLE RIGHT NOW
+'''
 def get_job_details_db(job_ids: np.ndarray) -> pd.DataFrame:
     if not np.any(job_ids):  # No job_ids
         return pd.DataFrame()
@@ -94,14 +99,16 @@ def get_job_details_db(job_ids: np.ndarray) -> pd.DataFrame:
         df_results = pd.read_sql_query(
             f"SELECT * FROM tbl_jobs WHERE job_id in {job_ids}", conn)
     return df_results
-
+'''
 
 # Insert scraped Linkedin into the database
+### NOT USING ORACLE RIGHT NOW
+'''
 def insert_job_detail_into_db(df_job_info: pd.DataFrame):
     if isinstance(df_job_info, pd.DataFrame) and not df_job_info.empty:
         df_job_info.to_sql('tbl_jobs', engine, 'appuser',
                            if_exists='append', index=False, method=None)
-
+'''
 
 # Scrape and Insert job details into AWS ETL:  SQS --> Lambda --> DynamoDB
 def push_job_ids_to_aws(job_ids: list):
